5fe47478b41c8dce2ab6cccd063b547fae851474
mkavinashkumar <m.k.avinashkumar@gmail.com>
2015-01-30 12:18:46 -0600

    comments for problems 2,3

--- highestProductOf3nlogn.java
+++ highestProductOf3nlogn.java
@@ -5,7 +5,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/*  Given an array_of_ints, find the highest_product you can get from three of the integers.
+The input array_of_ints will always have at least three integers. */
 
+
+// Code for nlogn execution time
 public class highestProductOf3nlogn {
 
 	public static void main(String[] args) throws FileNotFoundException,IOException {
--- productOfOtherNumbers.java
+++ productOfOtherNumbers.java
@@ -4,6 +4,16 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/* You have an array of integers, and for each index you want to find the product of every integer except the integer at that index.
+Write a function get_products_of_all_ints_except_at_index() that takes an array of integers and returns an array of the products.
+
+For example, given:
+
+  [1, 7, 3, 4]
+your function would return:
+
+  [84, 12, 28, 21] */
+
 
 public class productOfOtherNumbers {
 
